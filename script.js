a682cb3e4d00c31a86cbb2f32386d28e189b7fb1
Armix18 <armix182002@gmail.com>
2025-01-17 23:58:05 -0500

    feat: add ui opacity and ui design

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
 let container = document.querySelector("#container");
 container.classList.toggle('content');
-//console.log(container);
 
 let popup = document.querySelector("#popup");
 
-//let box;
-
 popup.addEventListener('click',(e) => {
-    container.textContent = '';
 
-    let squares = parseInt(prompt('Enter the number of squares', ));
-    let squaresDouble = Math.pow(squares, 2);
+    let squares = parseInt(prompt('Enter the number of grid (nxn)', ));
 
-    for (let i = 0; i < squaresDouble; i++) {
-        let box = document.createElement('div');
-        container.appendChild(box);
-        box.classList.add('box-grid');
-        //box.textContent = 'a';
-        //box.style.padding = (700/(squares*2))+ 'px';
-        let gapCalculation = (squares-1)*3;
-        box.style.flexBasis = (700 - gapCalculation)/squares + 'px';
-        //console.log(box.style.padding);
+    if(squares > 100){
+        alert('Enter a number less than 100');
+    } else {
+        container.textContent = '';
 
-        box.addEventListener('mouseleave',(e) => {
-            box.style.backgroundColor = 'rgba(110, 194, 166, 0.822)';
-        });
+        let squaresDouble = Math.pow(squares, 2);
+
+        drawGrid(squares, squaresDouble);
+
+
     }
 });
 
+setInterval(drawGrid, 1000);
+
+
+function drawGrid(squares, squaresDouble){
+    for (let i = 0; i < squaresDouble; i++) {
+
+        let box = document.createElement('div');
+
+        container.appendChild(box);
+        box.classList.add('box-grid');
+        
+        let gapCalculation = (squares-1)*2;
+        box.style.flexBasis = (700 - gapCalculation)/squares + 'px';
+
+        let classOpacity = 0;
+        box.style.opacity = 0;
+
+
+        //let j = 0;
+            box.addEventListener('mouseenter',(e) => {
+                //box.style.backgroundColor = 'rgba(110, 194, 166, 0.822)';
+                if(classOpacity < 1) {
+                classOpacity = classOpacity+0.1;
+                box.style.opacity = classOpacity;
+                console.log('opacity ' + classOpacity);
+
+                }
+            });
+            //j++;
+        
+    }
+}
 
 
